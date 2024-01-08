c2a29817f830ad794f4006a72490810a436f7175
hereWasKitus <nekitdragon@gmail.com>
2024-01-08 12:19:12 +0200

    Mobile height fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       id="app"
       className={clsx([
         "flex h-full max-h-[600px] w-full max-w-[940px] rounded-lg bg-white p-4 shadow-[0px_25px_40px_-20px] shadow-black/10",
-        "md:h-screen md:max-h-none md:flex-col md:bg-transparent md:p-0",
+        "md:max-h-none md:flex-col md:bg-transparent md:p-0",
       ])}
     >
       <Sidebar />
